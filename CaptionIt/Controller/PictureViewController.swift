2224e0d81a99b3f4ca20335e87f07f5f43c467c7
Korman Chen <kormanchen@Kormans-MacBook-Pro.local>
2018-01-20 18:53:52 -0800

    front-end update

--- CaptionIt/Controller/PictureViewController.swift
+++ CaptionIt/Controller/PictureViewController.swift
@@ -17,16 +17,8 @@ class PictureViewController: UIViewController, UIImagePickerControllerDelegate, 
     @IBOutlet weak var userImageView: UIImageView!
     let microsoftURL = "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/describe?"
 
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        getImageData()
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        
     }
     
     //MARK: Actions
@@ -65,18 +57,30 @@ class PictureViewController: UIViewController, UIImagePickerControllerDelegate, 
         present(alert, animated: true, completion: nil)
     }
     
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+    @objc func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         
         let image = info[UIImagePickerControllerOriginalImage] as! UIImage
         userImageView.image = image
+        getImageData()
+        if let data = UIImagePNGRepresentation(image) {
+            let filename = getDirectory().appendingPathComponent("picture.png")
+            try? data.write(to: filename)
+            getImageData()
+        }
+        
         dismiss(animated:true, completion: nil)
+    }
+    
+    func getDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
     }
     
     //MARK: Alamofire
     func getImageData() {
         
-        let params = ["maxCandidates" : "1"]
-        let headers: HTTPHeaders = ["Ocp-Apim-Subscription-Key" : "3ef12bba6b2040a2a06934fc85b83ae6", "url" : "https://i.ytimg.com/vi/SfLV8hD7zX4/maxresdefault.jpg"]
+        let params = ["maxCandidates" : "1", "url" : urlPath]
+        let headers: HTTPHeaders = ["Ocp-Apim-Subscription-Key" : "3ef12bba6b2040a2a06934fc85b83ae6", "Content-Type" : "application/json"]
         
         Alamofire.request(microsoftURL, method: .post, parameters: params, encoding: JSONEncoding.default, headers: headers).responseJSON { (response) in
             if response.result.isSuccess {
