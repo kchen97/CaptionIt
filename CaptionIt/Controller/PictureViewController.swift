9b09a8bd2dc5f5b7686fb56ed258e7a2114142b9
3kevind <kevdiec@gmail.com>
2018-01-20 23:25:48 -0800

    Add files via upload

--- CaptionIt/Controller/PictureViewController.swift
+++ CaptionIt/Controller/PictureViewController.swift
@@ -9,13 +9,17 @@
 import UIKit
 import Alamofire
 import SwiftyJSON
+import SVProgressHUD
+import ProgressHUD
 
 class PictureViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     //MARK: Properties
+
     @IBOutlet weak var captionLabel: UILabel!
     @IBOutlet weak var userImageView: UIImageView!
     let microsoftURL = "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/describe?"
+    let captionDataModel = CaptionDataModel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -62,32 +66,40 @@ class PictureViewController: UIViewController, UIImagePickerControllerDelegate, 
         let image = info[UIImagePickerControllerOriginalImage] as! UIImage
         userImageView.image = image
         getImageData()
-        if let data = UIImagePNGRepresentation(image) {
-            let filename = getDirectory().appendingPathComponent("picture.png")
-            try? data.write(to: filename)
-            getImageData()
-        }
-        
+        captionDataModel.reset()
         dismiss(animated:true, completion: nil)
-    }
-    
-    func getDirectory() -> URL {
-        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-        return paths[0]
     }
     
     //MARK: Alamofire
     func getImageData() {
+        SVProgressHUD.show()
         
-    let headers: HTTPHeaders = ["Ocp-Apim-Subscription-Key" : "3ef12bba6b2040a2a06934fc85b83ae6", "Content-Type" : "application/octet-stream"]
-    let imageData  = UIImageJPEGRepresentation(userImageView.image!, 1.0)!
+        let headers: HTTPHeaders = ["Ocp-Apim-Subscription-Key" : "3ef12bba6b2040a2a06934fc85b83ae6", "Content-Type" : "application/octet-stream"]
+        let imageData  = UIImageJPEGRepresentation(userImageView.image!, 1.0)!
 
         Alamofire.upload(imageData, to: microsoftURL, method: .post, headers: headers).responseJSON { (response) in
             if response.result.isSuccess {
-                print(response)
+                SVProgressHUD.dismiss()
+                let pictureJSON : JSON = JSON(response.result.value!)
+                print(pictureJSON)
+                self.captionDataModel.configure(pictureJSON)
+                self.updateUIWithCaptionData()
+
+            }
+            else {
+                SVProgressHUD.showError(withStatus: "An error occurred analyzing the image.")
+                SVProgressHUD.dismiss(withDelay: 1.50)
             }
         }
-        
     }
+    func updateUIWithCaptionData() {
+        if let caption = captionDataModel.getCaption() {
+            captionLabel.text = caption
+        }
+    }
+    @IBAction func getAnotherCaptionPressed(_ sender: UIBarButtonItem) {
+        updateUIWithCaptionData()
+    }
+
 }
 
